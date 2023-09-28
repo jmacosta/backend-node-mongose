20fdec3fbc1d5e5babda92ed2b07d0a758d8a749
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-09-28 19:20:59 +0200

    Add index to collection agentes

--- ejercicios/nodeapp/models/Agente.js
+++ ejercicios/nodeapp/models/Agente.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
 
 // definir el esquema de los agentes
-const agenteSchema = mongoose.Schema({
-  name: String,
-  age: Number,
-});
+const agenteSchema = mongoose.Schema(
+  {
+    name: { type: String, index: true },
+    age: { type: Number, index: true },
+  },
+  {
+    collection: "agentes",
+  }
+);
+//Tipo de métodos:
+// Estatico
+
+agenteSchema.statics.lista = function (filtro, skip, limit, sort, fields) {
+  const query = Agente.find(filtro);
+  //devuelve un objeto de tipo query q es un thenable
+  query.skip(skip);
+  query.limit(limit);
+  query.sort(sort);
+  query.select(fields);
+  return query.exec();
+};
+
+// método de instancia
+
+agenteSchema.methods.saluda = function () {
+  console.log("Hola soy el agente " + this.name);
+};
 
 // crear el modelo de agente
 
